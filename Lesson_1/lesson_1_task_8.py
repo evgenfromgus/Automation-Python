3d9c3f5e506d5435eefba211c45974df2bf54fa7
evgenfromgus <108752375+evgenfromgus@users.noreply.github.com>
2023-07-06 13:22:06 +0300

    Добавлены решения по заданиям первого урока
    Добавлены решения по заданиям первого урока

---
+++ Lesson_1/lesson_1_task_8.py
@@ -0,0 +1,13 @@
+def menya_pret(num):
+    print(num, end='')
+menya_pret(8)
+menya_pret(8)
+menya_pret(0)
+menya_pret(0)
+menya_pret(5)
+menya_pret(5)
+menya_pret(5)
+menya_pret(3)
+menya_pret(5)
+menya_pret(3)
+menya_pret(5)
