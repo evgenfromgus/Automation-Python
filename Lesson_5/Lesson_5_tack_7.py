19a026826590164c77048e256acd7debc595bf8a
evgenfromgus <108752375+evgenfromgus@users.noreply.github.com>
2023-07-09 15:38:42 +0300

    удалены из импорта не рабочие модули

--- Lesson_5/Lesson_5_tack_7.py
+++ Lesson_5/Lesson_5_tack_7.py
@@ -2,8 +2,6 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
