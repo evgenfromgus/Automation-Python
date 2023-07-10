35b4c1fde44a852a03c52a8c3a5fd2f99e9be99d
evgenfromgus <108752375+evgenfromgus@users.noreply.github.com>
2023-07-10 07:55:39 +0300

    удалены из импорта не рабочие библиотеки

--- Lesson_6/lesson_6_tack_2.py
+++ Lesson_6/lesson_6_tack_2.py
@@ -3,8 +3,6 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
 
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 chrome_options = webdriver.ChromeOptions()
