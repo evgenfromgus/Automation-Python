3f8f320c864f0345b835f77c8b0aa897239c2b5e
evgenfromgus <108752375+evgenfromgus@users.noreply.github.com>
2023-08-04 23:09:06 +0300

    изменено название функции

--- Complex/test_3.py
+++ Complex/test_3.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 from configuration import *
 
-def test_calculator_form(chrome_browser):
+def test_shop_form(chrome_browser):
     chrome_browser.get(URL_3)
     name = chrome_browser.find_element(By.ID, "user-name").send_keys("standard_user")
     pas = chrome_browser.find_element(By.ID, "password").send_keys("secret_sauce")
