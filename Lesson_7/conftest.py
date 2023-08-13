377f3f9917318bdfa2f33b256f70c251da432524
evgenfromgus <108752375+evgenfromgus@users.noreply.github.com>
2023-08-13 14:21:36 +0300

    урок 7

--- Lesson_7/conftest.py
+++ Lesson_7/conftest.py
@@ -2,11 +2,15 @@ import pytest
 from selenium import webdriver #импорт драйвера для взаимодействия с браузером
 from webdriver_manager.chrome import ChromeDriverManager # для установки без бинарного файла, наш драйвер
 from selenium.webdriver.chrome.service import Service as ChromeService #класс отвечающий за установку драйвера, закрытие и открытие браузера
-
+from selenium.webdriver.firefox.service import Service as FirefoxService
+from webdriver_manager.firefox import GeckoDriverManager
 @pytest.fixture()
-def chrome_browser2():
-    driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument("--window-size=1920,1080")
+def chrome_browsers():
+    #driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) #не работает с версией драйвера 115 и выше
+    # driver = webdriver.Chrome()
+    # chrome_options = webdriver.ChromeOptions()
+    # chrome_options.add_argument("--window-size=1920,1080")
+    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
+
     yield driver
     driver.quit()
--- Lesson_7/test/test_1.py
+++ Lesson_7/test/test_1.py
@@ -1,14 +1,15 @@
-from Lesson_7.pages.Main_page import MainPage
-from Lesson_7.pages.Data_fildes import DataFild
+from Lesson_7.pages.Mainpage import MainPage
+from Lesson_7.pages.Datafildes import DataFild
+from Lesson_7.conftest import *
 
 
-def test_assertion(chrome_browser2):
-    main_page = MainPage(chrome_browser2)
+def test_assertion(chrome_browsers):
+    main_page = MainPage(chrome_browsers)
     main_page.find_fields()
     main_page.filling_in_the_fields()
     main_page.click_submit_button()
 
-    data_fild = DataFild(chrome_browser2)
+    data_fild = DataFild(chrome_browsers)
     data_fild.find_fields()
     data_fild.get_class_first_name()
     data_fild.get_class_last_name()
@@ -20,7 +21,7 @@ def test_assertion(chrome_browser2):
     data_fild.get_class_company()
     data_fild.get_class_zipcode()
 
-    assert "danger" in data_fild.get_class_zipcode()
+    assert "success" in data_fild.get_class_first_name()
     assert "success" in data_fild.get_class_first_name()
     assert "success" in data_fild.get_class_last_name()
     assert "success" in data_fild.get_class_address()
@@ -29,3 +30,4 @@ def test_assertion(chrome_browser2):
     assert "success" in data_fild.get_class_country()
     assert "success" in data_fild.get_class_jobposition()
     assert "success" in data_fild.get_class_company()
+    assert "danger" in data_fild.get_class_zipcode()
