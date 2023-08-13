9483028cccce690888634110aefb704333dd5a90
evgenfromgus <108752375+evgenfromgus@users.noreply.github.com>
2023-08-14 00:53:30 +0300

    урок 7

---
+++ Lesson_7/test/test_2.py
@@ -0,0 +1,10 @@
+from Lesson_7.pages.Calcmainpage import CalcMain
+
+
+def test_calculator_assert(chrome_browsers):
+    calcmain = CalcMain(chrome_browsers)
+    calcmain.insert_time()
+    calcmain.clicking_buttons()
+    calcmain.wait_button_gettext()
+
+    assert "15" in calcmain.wait_button_gettext()
---
+++ Lesson_7/test/test_3.py
@@ -0,0 +1,18 @@
+from Lesson_7.pages.Shopmain import ShopmainPage
+from Lesson_7.pages.Shopcontainer import ShopContainer
+
+
+def test_shop(chrome_browsers):
+    expected_total = "58.29"
+    shopmain = ShopmainPage(chrome_browsers)
+    shopmain.registration_fields()
+    shopmain.buy_issue()
+    shopmain.click_issue()
+    shopmain.into_container()
+    container = ShopContainer(chrome_browsers)
+    container.checkout()
+    container.info()
+    container.price()
+
+    assert expected_total in container.price()  # Проверяем, что итоговая сумма равна $58.29
+    print("Итоговая сумма равна $58.29")  # Выводим сообщение в случае успешного выполнения
