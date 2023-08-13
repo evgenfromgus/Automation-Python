499e8c32d90ae5d7896c36814e37b3361a3c0666
evgenfromgus <108752375+evgenfromgus@users.noreply.github.com>
2023-08-13 14:55:31 +0300

    урок 7

--- Lesson_7/conftest.py
+++ Lesson_7/conftest.py
@@ -1,16 +1,15 @@
 import pytest
-from selenium import webdriver #импорт драйвера для взаимодействия с браузером
-from webdriver_manager.chrome import ChromeDriverManager # для установки без бинарного файла, наш драйвер
-from selenium.webdriver.chrome.service import Service as ChromeService #класс отвечающий за установку драйвера, закрытие и открытие браузера
-from selenium.webdriver.firefox.service import Service as FirefoxService
-from webdriver_manager.firefox import GeckoDriverManager
+from selenium import webdriver  # импорт драйвера для взаимодействия с браузером
+from selenium.webdriver.chrome.service import \
+    Service as ChromeService  # класс отвечающий за установку драйвера, закрытие и открытие браузера
+
+
 @pytest.fixture()
 def chrome_browsers():
-    #driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) #не работает с версией драйвера 115 и выше
-    # driver = webdriver.Chrome()
-    # chrome_options = webdriver.ChromeOptions()
-    # chrome_options.add_argument("--window-size=1920,1080")
-    driver = webdriver.Firefox(service=FirefoxService(GeckoDriverManager().install()))
-
+    # driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install())) #не работает с версией драйвера 115 и выше
+    service = ChromeService(executable_path='Lesson_7/chrome_driver/chromedriver.exe')
+    driver = webdriver.Chrome(service=service)
+    chrome_options = webdriver.ChromeOptions()
+    chrome_options.add_argument("--window-size=1920,1080")
     yield driver
     driver.quit()
